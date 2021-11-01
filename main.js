1b489e91d899a82010b26c94dbc7f5ebba1a7ccf
Damilare Fadodun <dhamilare27@gmail.com>
2021-11-01 04:55:06 +0100

    define collapsible with javaScript and css.

---
+++ main.js
@@ -0,0 +1,13 @@
+const collapsibles = document.querySelectorAll(".collapsible");
+collapsibles.forEach((item) =>
+  item.addEventListener("click", function () {
+    this.classList.toggle("collapsible--expanded");
+  })
+);
+
+// const collapsibles = document.querySelectorAll(".collapsible");
+// collapsibles.forEach((item) =>
+//   item.addEventListener("click", function () {
+//     this.classList.toggle("collapsible--expanded");
+//   })
+// );
